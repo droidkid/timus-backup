51ec10176ac2c4e436417bf531e0fdad9d89223d
droidkid <karch08@gmail.com>
2015-01-28 17:41:18 +0530

    Little more info in Readme added. The Script now downloads last AC of a user.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # timus-backup
+
+A simple script to backup the last AC solution of a user in the Timus Online Judge(acm.timus.ru). 
+Written in python3 and uses the 'requires' library.
+Not yet completed, currently only downloads last AC solution.
+
+TODO:
+    Backup last AC file of each problem from the TIMUS site.
+    Take JudgeID and password from config File.
+    Report error if authentication fails or network fails.
--- timus-backup.py
+++ timus-backup.py
@@ -5,30 +5,48 @@ import getpass;
 import requests;
 import re;
 
-def regexString(problemClass):
-    s = r'<TD CLASS="'+problemClass+r'"><A HREF="status\.aspx\?space=1&amp;num=([0-9]*)&amp;author=[0-9]*" TITLE="(.*)" REL="nofollow">\1</A></TD>'
-    return s;
+
+baseURL = 'http://acm.timus.ru/';
+
+
+def getSubmission(submIdURL, username, password, session):
+    u = baseURL+submIdURL;
+    print(u);
+    payload = {'Action':'getsubmit', 'JudgeID':username, 'Password':password};
+    r = session.post(u,payload);
+    print(r.text);
+
+def buildURL(page, params):
+    s = baseURL+page+".aspx?";
+    for param in params:
+        s = s + param +"="+params[param]+"&";
+    return (s[:-1]);
 
 #Get JudgeID and Password
 print("Enter JudgeID: ",end="", flush=True);
 judgeId = sys.stdin.readline().rstrip();
 password = getpass.getpass();
-judgeNumber = judgeId[:-2];
+judgeNumber = re.match(r'([0-9]*)', judgeId).group(1);
 
+params = {'author':judgeNumber, 'status':'accepted', 'refresh':'0', 'count':'100'};
+buildURL('status', params);
+
+u = buildURL('status',params);
 s = requests.session();
-r = s.get('http://acm.timus.ru/author.aspx?id='+judgeNumber);
-print("Receieved Problem List");
-#TODO: Check what happens if judgeID is invalid
+
+r = s.get(u);
+#TODO: what happens if judgeID is invalid ?
 
 
-accepted = regexString("accepted"); 
-tried = regexString("tried");
-empty = regexString("empty");
+submId_pattern = r'"(getsubmit\.aspx/[0-9]*\.[a-z]*)"';
+probId_pattern = r'"problem\.aspx\?space=1&amp;num=([0-9]*)"';
+probName_pattern = r'<SPAN CLASS="problemname">\. ([^<]*)</SPAN>';
 
-pattern = re.compile(accepted);
-print("List of Solved Questions: ");
-solved = 0;
-for (probId, probName) in pattern.findall(r.text):
-    print(probId+" "+probName);
-    solved=solved+1;
-print("Solved: "+str(solved));
+submId_re = re.compile(submId_pattern);
+probId_re = re.compile(probId_pattern);
+probName_re = re.compile(probName_pattern);
+
+submId = submId_re.findall(r.text);
+probId = probId_re.findall(r.text);
+probName = probName_re.findall(r.text);
+getSubmission(submId[0], judgeId, password, s);
